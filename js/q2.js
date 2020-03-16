9262fe0c6dca3388b73daaf32fc104186396f07f
yousef-97 <alshenyousef@gmail.com>
2020-03-16 12:31:44 +0200

    Q2-solved

---
+++ js/q2.js
@@ -0,0 +1,10 @@
+'use strict';
+function theStars(theNum){
+    var yousef= '';
+    for(var i = 0; i < theNum ; i++){
+        yousef+='*'
+        console.log(yousef)
+    }
+}
+testNum = 3;
+theStars(testNum);
